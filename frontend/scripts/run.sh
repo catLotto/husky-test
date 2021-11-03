c5b1da68c5f032acc7873fb60d9aa9324f71e131
Y.Y <colond@hashsnap.net>
2021-11-03 12:15:47 +0900

    update

--- frontend/scripts/run.sh
+++ frontend/scripts/run.sh
@@ -4,6 +4,7 @@ if test -f .server
 then
   echo 'restart server'
   npm run build
-  #pm2 start npm --name "next" -- start
+  pm2 delete -s next || :
+  pm2 start npm --name "next" -- start
   mkdir test
 fi
